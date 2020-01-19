d10ba40badfc990afc46c3ee80a1d54b2daffd24
SashaRevizov <35232695+SashaRevizov@users.noreply.github.com>
2020-01-19 19:21:21 +0200

    Delete keys.dev.js

--- config/keys.dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  MONGO_URL: 'mongodb+srv://keemerie:4ass55rf@cluster0-t548h.mongodb.net/test?retryWrites=true&w=majority',
-  SESSION_SECRET: 'secret',
-  SEND_GRID_KEY: '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',
-  MAIN_URL: 'http://localhost:3000',
-  MAIL: 'sasha160120@gmail.com'
-};
